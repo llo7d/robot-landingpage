ab6e9ca7699ac072b3b49bd3a48a660ff0e4b313
peterlloyd1 <63150386+llo7d@users.noreply.github.com>
2024-05-29 15:23:33 +0200

    Finished Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
 import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
     <>
       <Canvas>
+        <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
+        <mesh scale={(8, 8, 4)}>
           <boxGeometry />
           <meshStandardMaterial />
         </mesh>
