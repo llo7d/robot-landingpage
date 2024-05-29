0c826223b35732139edc954be640cd601df69fe5
peterlloyd1 <63150386+llo7d@users.noreply.github.com>
2024-05-29 22:18:24 +0200

    Adjusted milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,23 @@ import { OrbitControls } from '@react-three/drei';
 function App() {
   return (
     <>
-      <Canvas>
-        <OrbitControls />
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh scale={(8, 8, 4)}>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-      </Canvas>
+      <h1 className="text-3xl">Cute robot, Good ideas</h1>
+      <h1 className="text-3xl">just great</h1>
+      <div className="flex flex-col items-center justify-center">
+        <Canvas className="self-center">
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <mesh
+            scale={(2.5, 2.5, 2.5)}
+            position={[0, 0.8, 0]}
+            rotation={[0, 0.6, 0]}
+          >
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
+        <button className="mt-[-40%] text-2xl">Press me</button>
+      </div>
     </>
   );
 }
