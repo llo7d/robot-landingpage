a98d72ac0fb9bb7ec9f8a6aedd23e82d3b740fc0
peterlloyd1 <63150386+llo7d@users.noreply.github.com>
2024-06-04 22:50:12 +0200

    Milestone 3 done, but will need to redo as some bugs in robot source file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Robot } from './components/Robot';
 
 function App() {
   return (
@@ -10,10 +11,8 @@ function App() {
       <Canvas className="flex-grow">
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh scale={[2.5, 2.5, 2.5]} rotation={[0, 7, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
+
+        <Robot />
       </Canvas>
       <button className="mb-20">Press me</button>
     </div>
