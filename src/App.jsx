6d9e7d2b12cd78891f57f3a508ffff889a815fd7
peterlloyd1 <63150386+llo7d@users.noreply.github.com>
2024-06-13 23:21:08 +0200

    updated robot, looks good now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.css';
-import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Robot } from './components/Robot';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen justify-between items-center">
-      <h1 className="mt-10 text-xl">No Tutorials, No Courses</h1>
-      <h1 className="mt-2 text-xl">justcode</h1>
-      <Canvas className="flex-grow">
+    <div className="relative w-full h-screen">
+      <h1 className="absolute top-10 left-1/2 transform -translate-x-1/2 text-xl z-10">
+        No Tutorials, No Courses
+      </h1>
+      <h1 className="absolute top-20 left-1/2 transform -translate-x-1/2 text-xl z-10">
+        justcode
+      </h1>
+      <Canvas
+        className="absolute top-0 left-0 w-full h-full"
+        camera={{ fov: 45, position: [0, 0, 10] }}
+      >
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
 
         <Robot />
       </Canvas>
-      <button className="mb-20">Press mee</button>
+      <button className="absolute bottom-20 left-1/2 transform -translate-x-1/2 z-10">
+        Press me
+      </button>
     </div>
   );
 }
